0f0a9db6c420e2ea10a32bd6e98ea8450526a769
Jonathan E. Magen <yonkeltron@gmail.com>
2021-07-18 16:08:29 -0400

    many attempts to get chunk processing working
    Signed-off-by: Jonathan E. Magen <yonkeltron@gmail.com>

--- prep/mojo/parse_blank_line_separated.py
+++ prep/mojo/parse_blank_line_separated.py
@@ -1,4 +1,19 @@
+import uuid
+
 from mojo.collection import HaikuElement
 
-def chunk_to_haiku(chunk: str) -> HaikuElement:
-    pass
+
+def chunk_to_haiku(chunk: str, source: str) -> HaikuElement:
+    raw_lines = chunk.split("\n")
+
+    stripped_lines = [line.strip() for line in raw_lines]
+
+    lines = filter(lambda line: line != "", stripped_lines)
+
+    attrs = {
+        "lines": list(lines),
+        "uuid": str(uuid.uuid4()),
+        "source": source,
+    }
+
+    return HaikuElement.from_dict(attrs)
--- prep/tests/test_parse_blank_line_separated.py
+++ prep/tests/test_parse_blank_line_separated.py
@@ -1,5 +1,7 @@
 import pytest
 
+from mojo.parse_blank_line_separated import chunk_to_haiku
+
 def test_chunk_to_haiku() -> None:
     chunk = """
     Panda Bamboo
@@ -7,4 +9,8 @@ def test_chunk_to_haiku() -> None:
     Tree Flower
     """
 
-    
+    expected = ["Panda Bamboo", "Curry Noodle", "Tree Flower"]
+
+    haiku_element = chunk_to_haiku(chunk, "bogus source")
+
+    assert haiku_element.lines == expected
